cf4103e14a86157c3b417691d69167c03a635f70
JahirLarico <laricovargasjahir444@gmail.com>
2022-02-16 20:34:58 -0500

    Table Desing v1.2 and Logic

--- logic_draw_lines.js
+++ logic_draw_lines.js
@@ -1,11 +1,16 @@
 var dibujo = document.getElementById("dibujo");
 var linea = dibujo.getContext("2d");
-function dibujarlinea(color,xini,yini,xfin,yfin,grosor3=3)
+var n_lineas= document.querySelector(".lines");
+var draw = document.getElementById("draw");
+var position = document.getElementById("posicion");
+function dibujarlinea(xini,yini,xfin,yfin);
+const  teclas ={
+    UP:38,
+    DOWN:40,
+    LEFT:37,
+    RIGHT:39
+};
 {   
-    var xmax=dibujo.width;
-    var ymax=dibujo.height;
-    var xmin=0;
-    var ymin=0;
     linea.beginPath();
     linea.stronkeStyle =color;
     linea.lineWidth=grosor3;
