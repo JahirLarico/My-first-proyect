0061371b7efff5b25db173beced7d805e12d5d7c
JahirLarico <laricovargasjahir444@gmail.com>
2022-03-01 13:09:57 -0500

    Calculator Logic 99%

--- calculator.js
+++ calculator.js
@@ -20,8 +20,52 @@ const teclas={
 }
 function write(event){
     var codes=event.keyCode;
-    if(codes==teclas.number_zero){
-        result.innerHTML+="0";
-        console.log(event.keyCode)
-    }
+        if(codes==teclas.number_zero){
+            result.innerHTML+="0";
+        }
+        else if(codes==teclas.number_one){
+            result.innerHTML+="1";
+        }
+        else if(codes==teclas.number_two){
+            result.innerHTML+="2";
+        }
+        else if(codes==teclas.number_three){
+            result.innerHTML+="3";
+        }
+        else if(codes==teclas.number_four){
+            result.innerHTML+="4";
+        }
+        else if(codes==teclas.number_five){
+            result.innerHTML+="5";
+        }
+        else if(codes==teclas.number_six){
+            result.innerHTML+="6";
+        }
+        else if(codes==teclas.number_seven){
+            result.innerHTML+="7";
+        }
+        else if(codes==teclas.number_eight){
+            result.innerHTML+="8";
+        }
+        else if(codes==teclas.number_nine){
+            result.innerHTML+="9";
+        }
+        else if(codes==teclas.add){
+            result.innerHTML+="+";
+        }
+        else if(codes==teclas.subtract){
+            result.innerHTML+="-";
+        }
+        else if(codes==teclas.multiply){
+            result.innerHTML+="*";
+        }
+        else if(codes==teclas.divide){
+            result.innerHTML+="/";
+        }
+        else if(codes==teclas.decimal_point){
+            result.innerHTML+=".";
+        }   
+        else if(codes==teclas.total){
+            result.innerHTML+=document.cacl.txt.value=eval(cacl.txt.value);
+        }   
 }
